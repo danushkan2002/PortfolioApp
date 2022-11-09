696626e867afd16e7bf5f37510d8c5d0ef83e641
danushkanofficial <95530225+danushkanofficial@users.noreply.github.com>
2022-11-10 00:30:41 +0530

    domain

--- backend/settings.py
+++ backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f!28szh^r#q#yrl(hesafi2t&=vepusly33c@$xv&s@6iv!-2@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['difang.herokuapp.com']
 
 
 # Application definition
